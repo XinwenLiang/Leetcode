d2996853744e79069074fa535a2cbcc926a9caae
XinwenLiang <u7872714@anu.edu.au>
2024-11-26 09:05:53 +0800

    [fix] TreeNode [fix] PreorderTraversal [fix] PostorderTraversal [fix] InorderTraversal

---
+++ Day12/PostorderTraversal.java
@@ -0,0 +1,21 @@
+package Day12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorder(root, result);
+        return result;
+    }
+
+    public void postorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, result);
+        postorder(root.right, result);
+        result.add(root.val);
+    }
+}
